6930e008f2d1dac99f36dc298aee724cf2276a85
404Kurama <kurama.notfound@gmail.com>
2023-04-23 22:52:18 +0700

    feat(aboutpage): add check for update button

--- Bloxstrap/ViewModels/AboutViewModel.cs
+++ Bloxstrap/ViewModels/AboutViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System.Windows.Input;
-using CommunityToolkit.Mvvm.Input;
 using Bloxstrap.Helpers;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Bloxstrap.ViewModels
 {
     public class AboutViewModel
     {
         public string Version => $"Version {App.Version}";
+
+        public ICommand CFUWindowCommand => new RelayCommand(CheckForUpdate);
+
+        private void CheckForUpdate()
+        {
+            Updater.CheckForUpdate();
+        }
     }
 }
