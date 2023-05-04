cec5c58e7cc809abea16efa5343df3cd2843ff30
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-05-04 14:16:06 +0100

    Last minute fixes

--- Bloxstrap/Updater.cs
+++ Bloxstrap/Updater.cs
@@ -82,7 +82,7 @@ namespace Bloxstrap
             {
                 EventHandler ReleaseNotesLauncher = new((_, _) => Utilities.OpenWebsite($"https://github.com/{App.ProjectRepository}/releases/tag/v{currentVersionInfo.ProductVersion}"));
 
-                App.Notification.BalloonTipTitle = "Bloxstrap has been upgraded to v{currentVersionInfo.ProductVersion}";
+                App.Notification.BalloonTipTitle = $"Bloxstrap has been upgraded to v{currentVersionInfo.ProductVersion}";
                 App.Notification.BalloonTipText = "Click here to see what's new in this version";
                 App.Notification.BalloonTipClicked += ReleaseNotesLauncher;
                 App.Notification.ShowBalloonTip(30);
