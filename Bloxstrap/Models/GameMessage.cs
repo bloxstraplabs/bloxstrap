39d919811f1ff1914f5cedd370cda4ca6662f0d3
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-05-09 12:21:56 +0100

    Add support for game message communication (#183)
    game scripts can print a client message to send messages to bloxstrap, right now only for setting the presence status
    
    message is a json string formatted with the properties "command" and "data" that's prefixed with the string "[SendBloxstrapMessage]"
    
    example:
    print('[SendBloxstrapMessage] {"command":"SetPresenceStatus", "data":"hi"}')

---
+++ Bloxstrap/Models/GameMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bloxstrap.Models
+{
+    public class GameMessage
+    {
+        [JsonPropertyName("command")]
+        public string Command { get; set; } = null!;
+        
+        [JsonPropertyName("data")]
+        public string Data { get; set; } = null!;
+    }
+}
