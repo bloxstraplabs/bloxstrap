bb076da3665f778928758ee7f49b3b53cac4521b
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-05-12 09:48:11 +0100

    Match text as closely as possible

--- Bloxstrap/ViewModels/ByfronDialogViewModel.cs
+++ Bloxstrap/ViewModels/ByfronDialogViewModel.cs
@@ -8,10 +8,11 @@ namespace Bloxstrap.ViewModels
     public class ByfronDialogViewModel : FluentDialogViewModel, INotifyPropertyChanged
     {
         public string Version => $"Bloxstrap v{App.Version}";
+
         // Using dark theme for default values.
         public Thickness DialogBorder { get; set; } = new Thickness(0);
         public Brush Background { get; set; } = Brushes.Black;
-        public Brush Foreground { get; set; } = new SolidColorBrush(Color.FromRgb(216, 216, 216));
+        public Brush Foreground { get; set; } = new SolidColorBrush(Color.FromRgb(235, 235, 235));
         public Brush IconColor { get; set; } = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         public Brush ProgressBarBackground { get; set; } = new SolidColorBrush(Color.FromRgb(86, 86, 86));
         public ByfronDialogViewModel(IBootstrapperDialog dialog) : base(dialog)
