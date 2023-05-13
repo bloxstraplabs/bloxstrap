7e82dc4436df49d2a07e477763916635d7349ce9
1011025m <37438176+1011025m@users.noreply.github.com>
2023-05-13 10:31:05 +0800

    Didn't pass the -a flag in git...

--- Bloxstrap/ViewModels/ByfronDialogViewModel.cs
+++ Bloxstrap/ViewModels/ByfronDialogViewModel.cs
@@ -12,7 +12,7 @@ namespace Bloxstrap.ViewModels
         // Using dark theme for default values.
         public Thickness DialogBorder { get; set; } = new Thickness(0);
         public Brush Background { get; set; } = Brushes.Black;
-        public Brush Foreground { get; set; } = new SolidColorBrush(Color.FromRgb(235, 235, 235));
+        public Brush Foreground { get; set; } = new SolidColorBrush(Color.FromRgb(239, 239, 239));
         public Brush IconColor { get; set; } = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         public Brush ProgressBarBackground { get; set; } = new SolidColorBrush(Color.FromRgb(86, 86, 86));
         public ByfronDialogViewModel(IBootstrapperDialog dialog) : base(dialog)
