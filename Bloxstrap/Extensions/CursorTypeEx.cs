e3cf2ac3a2f07ac5d85e865d0f70f829bbfce798
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-07-15 13:18:39 +0100

    Minor visual adjustments

--- Bloxstrap/Extensions/CursorTypeEx.cs
+++ Bloxstrap/Extensions/CursorTypeEx.cs
@@ -9,8 +9,8 @@ namespace Bloxstrap.Extensions
         public static IReadOnlyDictionary<string, CursorType> Selections => new Dictionary<string, CursorType>
         {
             { "Default", CursorType.Default },
-            { "2013 - 2022", CursorType.From2013 },
-            { "2006 - 2013", CursorType.From2006 },
+            { "2013 (Angular)", CursorType.From2013 },
+            { "2006 (Cartoony)", CursorType.From2006 },
         };
     }
 }
