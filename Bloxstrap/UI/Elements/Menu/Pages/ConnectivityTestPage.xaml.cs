558df02f42360bca758863da77eb33848951efb2
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-07-27 00:16:19 +0100

    Add connectivity test
    lets see if this actually gets anywhere

---
+++ Bloxstrap/UI/Elements/Menu/Pages/ConnectivityTestPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Bloxstrap.UI.ViewModels.Menu;
+
+namespace Bloxstrap.UI.Elements.Menu.Pages
+{
+    /// <summary>
+    /// Interaction logic for ConnectivityTestPage.xaml
+    /// </summary>
+    public partial class ConnectivityTestPage
+    {
+        public ConnectivityTestPage()
+        {
+            DataContext = new ConnectivityTestViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bloxstrap/UI/ViewModels/Menu/ConnectivityTestViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace Bloxstrap.UI.ViewModels.Menu
+
+{
+    internal class ConnectivityTestViewModel : NotifyPropertyChangedViewModel
+    {
+        public ICommand RunAgainCommand => new RelayCommand(RunAgain);
+
+        public string Output { get; private set; } = "";
+
+        public ConnectivityTestViewModel()
+        {
+            Task.Run(() => RunTest());
+        }
+
+        private void RunAgain() => Task.Run(() => RunTest());
+
+        private void WriteToOutput(string message)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss");
+            Output += $"[{timestamp}] {message}\n";
+            OnPropertyChanged(nameof(Output));
+        }
+
+        private async void RunTest()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            Output = "";
+
+            try
+            {
+                string url = "https://www.google.com";
+                WriteToOutput($"Connecting to {url}...");
+                var result = await App.HttpClient.GetAsync(url);
+                WriteToOutput($"Connection complete! (HTTP {(int)result.StatusCode})");
+            }
+            catch (Exception ex)
+            {
+                WriteToOutput($"Connection failed!");
+                WriteToOutput(ex.ToString());
+            }
+
+            WriteToOutput("");
+
+            try
+            {
+                string url = "https://clientsettingscdn.roblox.com";
+                WriteToOutput($"Connecting to {url}...");
+                var result = await App.HttpClient.GetAsync(url);
+                string response = await result.Content.ReadAsStringAsync();
+                WriteToOutput($"Connection complete! (HTTP {(int)result.StatusCode})");
+                WriteToOutput(response);
+            }
+            catch (Exception ex)
+            {
+                WriteToOutput($"Connection failed!");
+                WriteToOutput(ex.ToString());
+            }
+
+            WriteToOutput("");
+
+            try
+            {
+                string url = "http://s3.amazonaws.com/setup.roblox.com/version";
+                WriteToOutput($"Connecting to {url}...");
+                var result = await App.HttpClient.GetAsync(url);
+                string response = await result.Content.ReadAsStringAsync();
+                WriteToOutput($"Connection complete! (HTTP {(int)result.StatusCode})");
+                WriteToOutput(response);
+            }
+            catch (Exception ex)
+            {
+                WriteToOutput($"Connection failed!");
+                WriteToOutput(ex.ToString());
+            }
+        }
+    }
+}
