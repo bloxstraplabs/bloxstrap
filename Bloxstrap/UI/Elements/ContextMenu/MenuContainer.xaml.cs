9a10be77bb42bdff51b3734ec33b3309cfa2ff62
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-10-10 22:04:17 +0100

    Adjust wording and look a little bit

--- Bloxstrap/UI/Elements/ContextMenu/MenuContainer.xaml.cs
+++ Bloxstrap/UI/Elements/ContextMenu/MenuContainer.xaml.cs
@@ -127,7 +127,7 @@ namespace Bloxstrap.UI.Elements.ContextMenu
         private void CloseRobloxMenuItem_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult result = Controls.ShowMessageBox(
-                "Are you sure you want to close Roblox?",
+                "Are you sure you want to close Roblox? This will forcefully end the process.",
                 MessageBoxImage.Warning,
                 MessageBoxButton.YesNo
             );
