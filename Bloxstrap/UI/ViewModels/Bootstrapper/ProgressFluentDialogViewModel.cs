1f46439e32f7ad6573edf3b787d3326a21830aaa
bluepilledgreat <97983689+bluepilledgreat@users.noreply.github.com>
2024-02-04 15:40:30 +0000

    better colours & alpha

--- Bloxstrap/UI/ViewModels/Bootstrapper/ProgressFluentDialogViewModel.cs
+++ Bloxstrap/UI/ViewModels/Bootstrapper/ProgressFluentDialogViewModel.cs
@@ -19,14 +19,14 @@ namespace Bloxstrap.UI.ViewModels.Bootstrapper
 
         public ProgressFluentDialogViewModel(IBootstrapperDialog dialog, bool aero) : base(dialog)
         {
-            const int alpha = 32;
+            const int alpha = 128;
 
             WindowBackdropType = aero ? BackgroundType.Aero : BackgroundType.Mica;
 
             if (aero)
                 BackgroundColourBrush = App.Settings.Prop.Theme.GetFinal() == Enums.Theme.Light ?
-                    new SolidColorBrush(Color.FromArgb(alpha, 255, 255, 255)) :
-                    new SolidColorBrush(Color.FromArgb(alpha, 0, 0, 0));
+                    new SolidColorBrush(Color.FromArgb(alpha, 225, 225, 225)) :
+                    new SolidColorBrush(Color.FromArgb(alpha, 30, 30, 30));
         }
     }
 }
