ee8890894001a83f05ffa20b855c1831166a321f
bluepilledgreat <97983689+bluepilledgreat@users.noreply.github.com>
2024-02-04 14:10:50 +0000

    add log and quiet check

--- Bloxstrap/App.xaml.cs
+++ Bloxstrap/App.xaml.cs
@@ -171,10 +171,13 @@ namespace Bloxstrap
 
                 if (!menuLock.IsAcquired)
                 {
-                    Frontend.ShowMessageBox(
-                        Bloxstrap.Resources.Strings.Menu_MenuAlreadyRunning,
-                        MessageBoxImage.Error
-                    );
+                    Logger.WriteLine(LOG_IDENT, "Could not acquire menu mutex");
+
+                    if (!LaunchSettings.IsQuiet)
+                        Frontend.ShowMessageBox(
+                            Bloxstrap.Resources.Strings.Menu_MenuAlreadyRunning,
+                            MessageBoxImage.Error
+                        );
                 }
                 else
                 {
