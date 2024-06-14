ec9c6002ed624b909b3291fc7cc28c974cab0c8b
Extravi <98912029+Extravi@users.noreply.github.com>
2024-06-14 12:46:36 -0400

    fix uninstall reg key for roblox

--- Bloxstrap/Bootstrapper.cs
+++ Bloxstrap/Bootstrapper.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+using System.Windows;
 using System.Windows.Forms;
 
 using Microsoft.Win32;
@@ -725,7 +725,7 @@ namespace Bloxstrap
             {
                 // revert launch uri handler to stock bootstrapper
 
-                string bootstrapperLocation = (string?)bootstrapperKey.GetValue("InstallLocation") + "RobloxPlayerLauncher.exe";
+                string bootstrapperLocation = (string?)bootstrapperKey.GetValue("InstallLocation") + "\\RobloxPlayerLauncher.exe";
 
                 ProtocolHandler.Register("roblox", "Roblox", bootstrapperLocation);
                 ProtocolHandler.Register("roblox-player", "Roblox", bootstrapperLocation);
