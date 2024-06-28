fdcc21de36908f635e6d72672df89c46d429a7a0
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2024-06-28 18:02:46 +0400

    Categorise FFlag presets

--- Bloxstrap/Resources/Strings.Designer.cs
+++ Bloxstrap/Resources/Strings.Designer.cs
@@ -1878,7 +1878,7 @@ namespace Bloxstrap.Resources {
         /// <summary>
         ///   Looks up a localized string similar to Fast Flags are extremely powerful, being that they are intended to only be used by Roblox engineers. While they can be very useful, manually configuring them can cause serious issues with stability and functionality. Do NOT use the editor if you are unsure of what you are doing.
         ///
-        ///Do NOT use this to import large &quot;flag lists&quot; made by other people that promise to improve performance or such. They are subject to the aforementioned issues, and they will cause problems for you since you will not be fully aware of [rest of string was truncated]&quot;;.
+        ///Do NOT use this to import large &quot;flag lists&quot; made by other people that promise to improve performance or such. They are subject to the aforementioned issues, and they will cause problems for you since you will not be fully aware  [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Menu_FastFlagEditor_Warning_Text {
             get {
@@ -1928,6 +1928,24 @@ namespace Bloxstrap.Resources {
         public static string Menu_FastFlags_Presets_AltGraphicsSelector_Title {
             get {
                 return ResourceManager.GetString("Menu.FastFlags.Presets.AltGraphicsSelector.Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Rendering and Graphics.
+        /// </summary>
+        public static string Menu_FastFlags_Presets_Categories_Rendering {
+            get {
+                return ResourceManager.GetString("Menu.FastFlags.Presets.Categories.Rendering", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to UI and Layout.
+        /// </summary>
+        public static string Menu_FastFlags_Presets_Categories_UserInterface {
+            get {
+                return ResourceManager.GetString("Menu.FastFlags.Presets.Categories.UserInterface", resourceCulture);
             }
         }
         
@@ -2112,7 +2130,7 @@ namespace Bloxstrap.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Choose what renderer Roblox should use. VR requires Direct3D/Automatic..
+        ///   Looks up a localized string similar to Choose which renderer Roblox should use..
         /// </summary>
         public static string Menu_FastFlags_Presets_RenderingMode_Description {
             get {
