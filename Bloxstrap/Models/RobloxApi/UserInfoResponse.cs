4acab1737e708bb449c5223308db2393c31b4f65
axell <mail@axell.me>
2024-07-05 23:38:12 +0200

    Create UserInfoResponse.cs

---
+++ Bloxstrap/Models/RobloxApi/UserInfoResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Bloxstrap.Models.RobloxApi
+{
+    /// <summary>
+    /// Roblox.Web.Responses.Users.UserInfoResponse
+    /// </summary>
+    public class UserInfoResponse
+    {
+        [JsonPropertyName("description")]
+        public string ProfileDescription { get; set; }
+
+        [JsonPropertyName("created")]
+        public string JoinDate { get; set; };
+
+        [JsonPropertyName("isBanned")]
+        public bool IsBanned { get; set; };
+
+        [JsonPropertyName("hasVerifiedBadge")]
+        public bool HasVerifiedBadge { get; set; };
+
+        [JsonPropertyName("name")]
+        public string Username { get; set; };
+
+        [JsonPropertyName("displayName")]
+        public string DisplayName { get; set; };
+    }
+}
