04c67ea661886c61adb7b0ead35c32b9a44a5db3
axell <mail@axell.me>
2024-07-06 18:51:40 +0200

    dont invert that (again)

--- Bloxstrap/UI/ViewModels/Menu/IntegrationsViewModel.cs
+++ Bloxstrap/UI/ViewModels/Menu/IntegrationsViewModel.cs
@@ -112,7 +112,7 @@ namespace Bloxstrap.UI.ViewModels.Menu
 
         public bool DiscordAccountOnProfile
         {
-            get => !App.Settings.Prop.AccountShownOnProfile;
+            get => App.Settings.Prop.AccountShownOnProfile;
             set => App.Settings.Prop.AccountShownOnProfile = value;
         }
 
