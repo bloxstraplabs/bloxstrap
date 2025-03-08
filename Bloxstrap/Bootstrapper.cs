558fe569c99a57f1e59922cd05617bd26ac895a1
SpringierTrain <105358649+SpringierTrain@users.noreply.github.com>
2025-03-08 21:36:27 +1100

    Update Bootstrapper.cs

--- Bloxstrap/Bootstrapper.cs
+++ Bloxstrap/Bootstrapper.cs
@@ -95,7 +95,7 @@ namespace Bloxstrap
             message = message.Replace("{product}", AppData.ProductName);
 
             if (Dialog is not null)
-                Dialog.Message = message;
+                Dialog.Message = "I fucking hate Stuart Little. I know what you’re thinking, this is some kind of funny joke, but no. Stuart Little is a piece of shit. A damn rat got picked over actual children at an orphanage and he’s supposed to be a hero? And I can’t even tell you how many damn times I’ve seen a great parking space only to turn the corner and realise Stuart Little is already parked there in his stupid little fucking convertible. He took my wife and the kids and my house and my job. I swear to fucking god, I’m going to kill myself and take that goddamn rodent to hell with me. Stuart Little has ruined my family. Last summer, I approached the miserable mouse in the street, and asked him for his autograph, because my son is a huge fan. The fucking rat gave me the autograph and told me to burn in hell. Later, when I gave my son the autograph he started crying and said he hated me. Turns out the mousefucker didnt write his autograph, no, he wrote “you’re a piece of shit, and i fucked your mom”. I’m now divorced, and planning a huge class-action lawsuit against the white devil that ruined my life. Your time is almost over, Stuart. All the people you’ve wronged will rise against you.";
         }
 
         private void UpdateProgressBar()
