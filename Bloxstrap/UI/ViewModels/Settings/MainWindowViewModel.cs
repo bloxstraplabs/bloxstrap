d79f947d36785e923e51f64a26f54809d2095a45
return_request <wirek5045@gmail.com>
2025-03-18 21:36:01 +0100

    improve save and launch

--- Bloxstrap/UI/ViewModels/Settings/MainWindowViewModel.cs
+++ Bloxstrap/UI/ViewModels/Settings/MainWindowViewModel.cs
@@ -69,7 +69,10 @@ namespace Bloxstrap.UI.ViewModels.Settings
         public void SaveAndLaunchSettings()
         {
             SaveSettings();
-            LaunchHandler.LaunchRoblox(LaunchMode.Player);
+            if (!App.LaunchSettings.TestModeFlag.Active) // test mode already launches an instance
+                LaunchHandler.LaunchRoblox(LaunchMode.Player);
+
+            CloseWindow();
         }
     }
 }
