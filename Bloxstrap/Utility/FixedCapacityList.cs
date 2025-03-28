c1842c044346bee6448eef06bc1ea04cda17cf49
Matt <97983689+bluepilledgreat@users.noreply.github.com>
2025-03-28 19:33:51 +0000

    Replace AssetDelivery API with Thumbnails API for Discord RPC images (#4947)
    * replace assetdelivery with thumbnails for rpc
    
    * update GetThumbnailUrlAsync logging
    
    * fix build error

---
+++ Bloxstrap/Utility/FixedCapacityList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloxstrap.Utility
+{
+    internal class FixedSizeList<T> : List<T>
+    {
+        public int MaxSize { get; }
+
+        public FixedSizeList(int size)
+        {
+            MaxSize = size;
+        }
+
+        public new void Add(T item)
+        {
+            if (Count >= MaxSize)
+                RemoveAt(Count - 1);
+            base.Add(item);
+        }
+    }
+}
