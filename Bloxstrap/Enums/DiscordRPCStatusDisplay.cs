e2c50167114cab0497a2cc0865329a0d34ccb308
SuperCater <supercater500@gmail.com>
2025-07-28 17:13:36 -0400

    code styling fix

--- Bloxstrap/Enums/DiscordRPCStatusDisplay.cs
+++ Bloxstrap/Enums/DiscordRPCStatusDisplay.cs
@@ -1,15 +1,19 @@
-namespace Bloxstrap.Enums;
 using DiscordRPC;
-/// <summary>
-/// Represents the display options for Discord RPC status.
-/// </summary>
-/// <remarks>
-/// This is basically just a copy of the enum from the DiscordRPC library but with attributes added for the UI
-/// </remarks>
-public enum DiscordRPCStatusDisplay {
-	[EnumName(FromTranslation = "Enums.DiscordRPCStatusDisplay.Name")]
-	Name = StatusDisplayType.Name,
-	[EnumName(FromTranslation = "Enums.DiscordRPCStatusDisplay.Details")]
-	Details = StatusDisplayType.Details,
-	// State also exists, but I don't think it makes sense to use it here
+
+namespace Bloxstrap.Enums
+{
+	/// <summary>
+	/// Represents the display options for Discord RPC status.
+	/// </summary>
+	/// <remarks>
+	/// This is basically just a copy of the enum from the DiscordRPC library but with attributes added for the UI
+	/// </remarks>
+	public enum DiscordRPCStatusDisplay 
+	{
+		[EnumName(FromTranslation = "Enums.DiscordRPCStatusDisplay.Name")]
+		Name = StatusDisplayType.Name,
+		[EnumName(FromTranslation = "Enums.DiscordRPCStatusDisplay.Details")]
+		Details = StatusDisplayType.Details,
+		// State also exists, but I don't think it makes sense to use it here
+	}
 }
