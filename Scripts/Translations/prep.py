a0de67700d1851d5aba0999edfc2c15a87f8c84a
Otsako <88578103+otsako-vpk@users.noreply.github.com>
2026-04-03 21:45:52 +0200

    Fix chinese translations (#6375)
    * fix chinese translations
    
    * remove obsolete upgrading logic
    
    locale gets set to nil before it can even run, and i cant be bothered
    
    * update prep.py to use the new locale codes

--- Scripts/Translations/prep.py
+++ Scripts/Translations/prep.py
@@ -3,9 +3,16 @@ import glob, shutil, re
 exports = input("Path of folder of exported Crowdin files: ")
 dest = input("Destination resources folder: ")
 
+icu_codes = {
+	"zh-CN": "zh-Hans-CN",
+	"zh-HK": "zh-Hant-HK",
+	"zh-TW": "zh-Hant-TW"
+}
+
 for filename in glob.glob(f"{exports}\\**\\*.*", recursive=True):
 	print(f"Copying {filename}")
 
 	localeCode = re.search("\\\\([a-zA-Z\\-]+)\\\\Strings.", filename).group(1)
+	localeCode = icu_codes.get(localeCode, localeCode)
 
 	shutil.copy(filename, dest + f"\\Strings.{localeCode}.resx")
