faa6a6ca8e8c5a2b5dd3f0cebb05f308fe1bdff1
Alexa <67791946+GoingCrazyDude@users.noreply.github.com>
2026-04-05 21:23:51 +0530

    Add MediaElement support

--- Bloxstrap/UI/Elements/Bootstrapper/CustomDialog.Elements.cs
+++ Bloxstrap/UI/Elements/Bootstrapper/CustomDialog.Elements.cs
@@ -638,6 +638,35 @@ namespace Bloxstrap.UI.Elements.Bootstrapper
             return image;
         }
 
+        private static UIElement HandleXmlElement_MediaElement(CustomDialog dialog, XElement xmlElement)
+        {
+            var media = new MediaElement();
+            HandleXmlElement_FrameworkElement(dialog, media, xmlElement);
+
+            RenderOptions.SetBitmapScalingMode(media, BitmapScalingMode.HighQuality); // i think letting this be modifiable would be funny
+
+            // should behaviour be modifiable? (except unloadedbehavior ig)
+            media.LoadedBehavior = MediaState.Play;
+            media.UnloadedBehavior = MediaState.Close;
+
+            media.Volume = ParseXmlAttribute<double>(xmlElement, "Volume", 1);
+
+            media.Stretch = ParseXmlAttribute<Stretch>(xmlElement, "Stretch", Stretch.Uniform);
+            media.StretchDirection = ParseXmlAttribute<StretchDirection>(xmlElement, "StretchDirection", StretchDirection.Both);
+
+            media.Source = GetMediaSourceData(dialog, "Source", xmlElement);
+
+            if (ParseXmlAttribute<bool>(xmlElement, "Looped", true))
+            {
+                media.MediaEnded += (Sender, e) =>
+                {
+                    media.Position = TimeSpan.Zero;
+                };
+            }
+
+            return media;
+        }
+
         private static RowDefinition HandleXmlElement_RowDefinition(CustomDialog dialog, XElement xmlElement)
         {
             var rowDefinition = new RowDefinition();
