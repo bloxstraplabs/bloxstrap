d7d1548f055881678566370e418e2a9f1e6e654a
Ryan Luu <ryan.luu@ryanluu.dev>
2026-04-30 16:56:00 -0700

    Add 2025 Roblox Player icon

--- Bloxstrap/Extensions/BootstrapperIconEx.cs
+++ Bloxstrap/Extensions/BootstrapperIconEx.cs
@@ -7,6 +7,7 @@ namespace Bloxstrap.Extensions
         public static IReadOnlyCollection<BootstrapperIcon> Selections => new BootstrapperIcon[]
         {
             BootstrapperIcon.IconBloxstrap,
+            BootstrapperIcon.Icon2025,
             BootstrapperIcon.Icon2022,
             BootstrapperIcon.Icon2019,
             BootstrapperIcon.Icon2017,
@@ -62,6 +63,7 @@ namespace Bloxstrap.Extensions
                 BootstrapperIcon.Icon2017 => Properties.Resources.Icon2017,
                 BootstrapperIcon.Icon2019 => Properties.Resources.Icon2019,
                 BootstrapperIcon.Icon2022 => Properties.Resources.Icon2022,
+                BootstrapperIcon.Icon2025 => Properties.Resources.Icon2025,
                 BootstrapperIcon.IconBloxstrapClassic => Properties.Resources.IconBloxstrapClassic,
                 _ => Properties.Resources.IconBloxstrap
             };
